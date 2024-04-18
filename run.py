2f447bf9ed2d20d07190c619a4de6c67820ef1ed
diegolfor9 <pablovp-8@hotmail.com>
2024-04-18 16:19:39 +0200

    New update

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import pickle
 import torch
@@ -16,7 +17,7 @@ def make_prediction(model, X, scaler_y):
 
 if __name__ == "__main__":
     # Rutas a los archivos necesarios
-    input_csv = 'dyrk1a_molecules_evaluation/candidates_1.csv'
+    input_txt_path = 'dyrk1a_molecules_evaluation/RGA53.txt'
     model_path = 'models/dyrk1a/gaussian_dyrk1a_model.pkl'
     scaler_path = 'models/dyrk1a/gaussian_dyrk1a_scaler.pkl'
 
@@ -24,14 +25,16 @@ if __name__ == "__main__":
     model = load_model(model_path)
     scaler_y = load_model(scaler_path)
 
-    # Cargar datos
-    df = pd.read_csv(input_csv)
+    # Cargar datos desde un archivo TXT
+    with open(input_txt_path, 'r') as file:
+        smiles_list = file.read().splitlines()
+    df = pd.DataFrame(smiles_list, columns=['SMILES'])
 
     # Definir el dispositivo
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     # Preparar los datos
-    X = build_dataset(df, device=device, include_y=False)  # Asume que devuelve un array de NumPy
+    X = build_dataset(df, include_y=False)  # Asume que devuelve un array de NumPy
 
     # Hacer predicciones
     predictions = make_prediction(model, X, scaler_y)
@@ -39,8 +42,10 @@ if __name__ == "__main__":
     # Preparar DataFrame de salida incluyendo SMILES
     output_df = pd.DataFrame({'SMILES': df['SMILES'], 'pChEMBL Predicted': predictions})
     
-    # Guardar las predicciones en un archivo CSV
-    output_csv = 'dyrk1a_molecules_evaluation/candidates_1_predictions.csv'
-    output_df.to_csv(output_csv, index=False)
-    print("Predicciones guardadas con éxito en", output_csv)
-
+    # Definir el nombre del archivo de salida basado en el nombre del archivo de entrada
+    # Extraer el nombre del archivo sin la ruta ni la extensión
+    base_name = os.path.splitext(os.path.basename(input_txt_path))[0]
+    # Formar el nuevo nombre del archivo de salida
+    output_filename = os.path.join(os.path.dirname(input_txt_path), f'{base_name}_affinity.csv')
+    output_df.to_csv(output_filename, index=False)
+    print("Predicciones guardadas con éxito en", output_filename)
